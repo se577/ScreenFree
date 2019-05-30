c94734c5310c1e6d9c0c5da9e6e3653074b09416
Patrick Stewart <patrick.stewart101@gmail.com>
2019-05-30 18:01:20 -0400

    Initial Commit

---
+++ ScreenFree/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  ScreenFree
+//
+//  Created by Patrick Stewart on 5/30/19.
+//  Copyright © 2019 TrickStewart. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
